e310b93de941f64403ee4a98477f501c1ba6f16c
apfdamascena <apfd@cin.ufpe.br>
2020-08-04 02:07:58 -0300

    starting Teachers page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/images/logo.svg';
 import landing from '../../assets/images/landing.svg';
@@ -24,15 +25,15 @@ export default function Landing(){
                 />
 
                 <div className= "buttons-container">
-                    <a href = "#" className= "study">
+                    <Link to = "/study" className= "study">
                         <img src = {study} alt = "Estudar"/>
                         Estudar
-                    </a>
+                    </Link>
 
-                    <a href = "#" className= "give-classes">
+                    <Link to = "/give-classes" className= "give-classes">
                         <img src = {giveClasses} alt = "Dar aulas"/>
                         Dar aulas
-                    </a>
+                    </Link>
                 </div>
 
                 <span className = "total-connections">
--- src/pages/Teachers/index.tsx
+++ src/pages/Teachers/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './teachers.css';
+
+import backIcon from '../../assets/images/icons/back.svg';
+import logo from '../../assets/images/logo.svg';
 
 export default function Teachers() {
     return (
-        <h1>teachers!!</h1>
+        <div id="page-teacher-list" className = "container">
+            <header className = "page-header">
+                <div className = "top-bar-container">
+                    <Link to = "/">
+                        <img src = {backIcon} alt = "Voltar"/> 
+                    </Link>
+                    <img src = {logo} alt = "Logo Proffy"/>
+                </div>
+
+                <div className = "header-content">
+                    <strong>Estes são os proffys disponíveis.</strong>
+                </div>
+
+            </header>
+        </div>
     );
 }
