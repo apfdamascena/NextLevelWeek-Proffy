ce54e9ad3cc6970700d87efade218ba3de0f9b56
apfdamascena <apfd@cin.ufpe.br>
2020-08-04 20:40:23 -0300

    starting express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,9 @@
-console.log("Hello world");
+import express from 'express';
+
+const APP = express();
+
+APP.get('/users', (request, response) => {
+    return response.send("hello world");
+});
+
+APP.listen(2608);
