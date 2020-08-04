dd97c28ebf8b7f10a53068f4ea6a732f8de1eb13
apfdamascena <apfd@cin.ufpe.br>
2020-08-04 16:19:51 -0300

    structure teacher list jsx done

--- src/pages/Teachers/index.tsx
+++ src/pages/Teachers/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import './teachers.css';
+import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
 
 export default function Teachers() {
     return (
@@ -26,7 +27,31 @@ export default function Teachers() {
 
             <main>
                 <article className= "teacher-item">
-                    
+                    <header>
+                    <img src = "https://avatars1.githubusercontent.com/u/52205263?s=400&u=033c62df4cc4f73b3010473faf859919019679af&v=4" alt = "Alex Image" />
+                    <div>
+                        <strong>Alex Damascena</strong>
+                        <span>Matemática</span>
+                    </div>
+                    </header>
+
+                    <p>
+                        Estude até falar: eu nao aguento mais.
+                        <br/> <br/>
+                        Tentando aprender algo sempre que possível. Falar que "sabe" é apenas um bloqueio
+                        mental para melhorar ainda mais. Esteja sempre no topo e lá sempre terá uma plateia para você incentivar.
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src = {whatsAppIcon} alt= "Whatsapp: Entrar em contato" />
+                            Entrar em contato
+                        </button>
+                    </footer>
                 </article>
             </main>
         </div>
