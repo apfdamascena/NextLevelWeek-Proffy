8cfe97da34bea88b4705badcc4cbaffd58751b19
apfdamascena <apfd@cin.ufpe.br>
2020-08-05 16:18:42 -0300

    API done

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import ClassControler from './controllers/classesController';
+import ConnectionsController from './controllers/connectionsControler';
 
 const routes = express.Router();
 const classesControllers = new ClassControler();
+const connectionsControllers = new ConnectionsController();
 
 
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
+
+routes.post('/connections', connectionsControllers.create);
+routes.get('/connections', connectionsControllers.index);
 
 export default routes;
