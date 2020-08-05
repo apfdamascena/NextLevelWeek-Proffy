e2a1f46f251629324e559a430f0d5ed4a8c2b03b
apfdamascena <apfd@cin.ufpe.br>
2020-08-05 18:50:40 -0300

    teachersForm almost done

--- src/pages/TeachersForm/index.tsx
+++ src/pages/TeachersForm/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
 import './teachersForm.css';
+import Input from '../../components/Input';
+import warningIcom from '../../assets/images/icons/warning.svg';
 
 export default function TeachersForm(){
     return(
@@ -15,22 +17,31 @@ export default function TeachersForm(){
                 <fieldset>
                     <legend>Seus dados</legend>
 
-                    <div className="input-block">
-                        <label htmlFor="name">Nome completo</label>
-                        <input type = "text" id = "name"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="avatar">Avatar</label>
-                        <input type = "text" id = "avatar"/>
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="whatsapp">Whatsapp</label>
-                        <input type = "text" id = "whatsapp"/>
-                    </div>
+                    <Input name="name" label = "Nome completo"/>
+                    <Input name="avatar" label = "Avatar"/>
+                    <Input name="whatsapp" label = "Whatsapp"/>
 
                 </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Input name="subject" label = "Nome completo"/>
+                    <Input name="cost" label = "Custo da sua hora por aula"/>
+                    <Input name="whatsapp" label = "Whatsapp"/>
+
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src = {warningIcom} alt = "Aviso importante" />
+                        Importante! <br/> 
+                        Preencha todos os dados
+                    </p>
+                    <button type = "button">
+                        Salvar cadastro
+                    </button>
+                </footer>
             </main>
         </div>
 
