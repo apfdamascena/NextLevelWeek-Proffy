d08cb7dc920a64565d526368886c880545021ca6
apfdamascena <apfd@cin.ufpe.br>
2020-08-05 15:51:21 -0300

    routes almost done

--- server/src/controllers/classesController.ts
+++ server/src/controllers/classesController.ts
@@ -25,8 +25,18 @@ export default class ClassControler {
         const timeMinutes = convertHourToMinutes(time);
 
         const classes = await database('classes')
+            .whereExists(function(){
+                this.select('class_schedule.*')
+                .from('class_schedule')
+                .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+                .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+                .whereRaw('`class_schedule`.`from` <= ??',[timeMinutes])
+                .whereRaw('`class_schedule`.`to` > ??',[timeMinutes])
+            })
             .where('classes.subject', '=', subject)
-        return response.send();
+            .join('users', 'classes.user_id', '=', 'users.id')
+            .select(['classes.*', 'users.*']);
+        return response.json(classes);
     }
     
     async create(request: Request, response: Response){
