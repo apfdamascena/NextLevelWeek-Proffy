8e09785bed4d1cf7ca8d4b447e09370d13114b12
apfdamascena <apfd@cin.ufpe.br>
2020-08-04 21:19:21 -0300

    making routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 
 const APP = express();
+APP.use(express.json());
 
-APP.get('/users', (request, response) => {
-    return response.send("hello world");
+//get -> Buscar ou listar uma info
+// post -> criar uma info
+// put: atualizar uma info existente
+// delete: deletar uma info existente
+
+//corpo (request body): dados para criacao ou atualizacao de um registro
+// route params: identificar qual recurso eu quero atualizar ou deletar
+// query params: paginacao, filtros, ordenacao
+
+APP.post('/users', (request, response) => {
+    return response.json({ message: "hellow world"});
 });
 
 APP.listen(2608);
