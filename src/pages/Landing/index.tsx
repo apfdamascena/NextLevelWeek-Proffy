79c11bec0f1585b3db4faeb4154a6af975802975
apfdamascena <apfd@cin.ufpe.br>
2020-08-05 22:21:44 -0300

    connect landing page front/back

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../../assets/images/logo.svg';
@@ -8,8 +8,17 @@ import giveClasses from '../../assets/images/icons/give-classes.svg';
 import purpleHeart from '../../assets/images/icons/purple-heart.svg';
 
 import './landing.css';
+import API from '../../services/api';
 
 export default function Landing(){
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        API.get('connections').then((response) => {
+            const {total} = response.data
+            setTotalConnections(total);
+        })
+    }, [])
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -37,7 +46,7 @@ export default function Landing(){
                 </div>
 
                 <span className = "total-connections">
-                    Total de 200 conexões já realizadas <img src = {purpleHeart} alt = "purple heart" />
+                    Total de {totalConnections} conexões já realizadas <img src = {purpleHeart} alt = "purple heart" />
                 </span>
             </div>
         </div>
