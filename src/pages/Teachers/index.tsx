4162b5fbb1f8c12aa60893e029a49d31b4972b2e
apfdamascena <apfd@cin.ufpe.br>
2020-08-05 21:13:52 -0300

    refactoring some code and adding new features

--- src/pages/Teachers/index.tsx
+++ src/pages/Teachers/index.tsx
@@ -3,20 +3,49 @@ import PageHeader from '../../components/PageHeader';
 import './teachers.css';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function Teachers() {
     return (
-        <div id="page-teacher-list" className = "container">
-            <PageHeader title = "Estes são os proffys disponíveis.">
-                <form id = "search-teachers">
-                    <Input name= "subject" label = "Matéria" />
-                    <Input name= "week_day" label = "Dia da semana" />
-                    <Input type = "time" name = "time" label = "Hora" />
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis.">
+                <form id="search-teachers">
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Matematica', label: 'Matematica' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'Fisica', label: 'Fisica' },
+                            { value: 'Filosofia', label: 'Filosofia' },
+                            { value: 'Sociologia', label: 'Sociologia' },
+                            { value: 'Historia', label: 'Historia' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Química', label: 'Química' },
+                        ]}
+                    />
+
+                    <Select
+                        name="week_day"
+                        label="Dia da semana"
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado' }
+                        ]}
+                    />
+                    <Input type="time" name="time" label="Hora" />
                 </form>
             </PageHeader>
 
             <main>
-                <TeacherItem/>
+                <TeacherItem />
             </main>
         </div>
     );
