17a5d9b9658f24e84445b8805f3d984ce6b749f9
apfdamascena <apfd@cin.ufpe.br>
2020-08-05 21:36:26 -0300

    adding state

--- src/pages/TeachersForm/index.tsx
+++ src/pages/TeachersForm/index.tsx
@@ -7,54 +7,80 @@ import warningIcom from '../../assets/images/icons/warning.svg';
 import Textarea from '../../components/TextArea';
 import Select from '../../components/Select';
 
-export default function TeachersForm(){
-    return(
-        <div id="page-teacher-form" className = "container">
-            <PageHeader 
-                title = "Que incrivel que voce quer dar aulas."
-                description = "O primeiro passo é preencher esse formulário de inscrição"
+export default function TeachersForm() {
+    return (
+        <div id="page-teacher-form" className="container">
+            <PageHeader
+                title="Que incrivel que voce quer dar aulas."
+                description="O primeiro passo é preencher esse formulário de inscrição"
             />
 
             <main>
                 <fieldset>
                     <legend>Seus dados</legend>
 
-                    <Input name="name" label = "Nome completo"/>
-                    <Input name="avatar" label = "Avatar"/>
-                    <Input name="whatsapp" label = "Whatsapp"/>
-                    <Textarea name = "bio" label = "Biografia"/>
+                    <Input name="name" label="Nome completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="Whatsapp" />
+                    <Textarea name="bio" label="Biografia" />
                 </fieldset>
 
                 <fieldset>
                     <legend>Sobre a aula</legend>
 
-                    <Select 
+                    <Select
                         name="subject"
-                        label = "Matéria"
-                        options = {[
-                            {value: 'Artes', label: 'Artes'},
-                            {value: 'Biologia', label: 'Biologia'},
-                            {value: 'Matematica', label: 'Matematica'},
-                            {value: 'Geografia', label: 'Geografia'},
-                            {value: 'Fisica', label: 'Fisica'},
-                            {value: 'Filosofia', label: 'Filosofia'},
-                            {value: 'Sociologia', label: 'Sociologia'},
-                            {value: 'Historia', label: 'Historia'},
-                            {value: 'Português', label: 'Português'},
-                            {value: 'Química', label: 'Química'},
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Matematica', label: 'Matematica' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'Fisica', label: 'Fisica' },
+                            { value: 'Filosofia', label: 'Filosofia' },
+                            { value: 'Sociologia', label: 'Sociologia' },
+                            { value: 'Historia', label: 'Historia' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Química', label: 'Química' },
                         ]}
                     />
-                    <Input name="cost" label = "Custo da sua hora por aula"/>
+                    <Input name="cost" label="Custo da sua hora por aula" />
 
+                </fieldset>
+
+                <fieldset>
+                    <legend>Horários disponíveis
+                    <button type="button">
+                            Novo horário
+                    </button>
+                    </legend>
+
+                    <div className="schedule-item">
+                        <Select
+                            name="week_day"
+                            label="Dia da semana"
+                            options={[
+                                { value: '0', label: 'Domingo' },
+                                { value: '1', label: 'Segunda-feira' },
+                                { value: '2', label: 'Terça-feira' },
+                                { value: '3', label: 'Quarta-feira' },
+                                { value: '4', label: 'Quinta-feira' },
+                                { value: '5', label: 'Sexta-feira' },
+                                { value: '6', label: 'Sábado' }
+                            ]}
+                        />
+                        <Input name = "from" label = "Das" type="time"/>
+                        <Input name = "to" label = "Até" type="time"/>
+                    </div>
                 </fieldset>
 
                 <footer>
                     <p>
-                        <img src = {warningIcom} alt = "Aviso importante" />
-                        Importante! <br/> 
+                        <img src={warningIcom} alt="Aviso importante" />
+                        Importante! <br />
                         Preencha todos os dados
                     </p>
-                    <button type = "button">
+                    <button type="button">
                         Salvar cadastro
                     </button>
                 </footer>
