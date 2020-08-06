3d569ef384926e509f8a6c93fa7f944b1e9a6842
apfdamascena <apfd@cin.ufpe.br>
2020-08-06 20:34:22 -0300

    adding icons to study Pages

--- mobile/src/routes/studyTabs.tsx
+++ mobile/src/routes/studyTabs.tsx
@@ -1,15 +1,60 @@
 import React from 'react';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TeacherList from '../Pages/TeacherList';
 import Favorite from '../Pages/Favorites';
+import { Ionicons } from '@expo/vector-icons'
 
-const {Navigator, Screen} = createBottomTabNavigator();
+const { Navigator, Screen } = createBottomTabNavigator();
 
-export default function StudyTabs(){
-    return(
-        <Navigator>
-            <Screen name = "TeacherList" component={TeacherList}></Screen>
-            <Screen name = "Favorite" component= {Favorite}></Screen>
+export default function StudyTabs() {
+    return (
+        <Navigator
+            tabBarOptions={{
+                style: {
+                    elevation: 0,
+                    shadowOpacity: 0,
+                    height: 100,
+                },
+
+                tabStyle: {
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                },
+
+                iconStyle: {
+                    flex: 0,
+                    width: 20,
+                    height: 20,
+                },
+
+                labelStyle: {
+                    fontFamily: 'Archivo_700Bold',
+                    fontSize: 13,
+                    marginLeft: 16,
+                },
+                inactiveBackgroundColor: "#FAFAFC",
+                activeBackgroundColor: "#EBEBF5",
+                inactiveTintColor: "#C1BCCC",
+                activeTintColor: "#32264D",
+            }}
+        >
+            <Screen name="TeacherList" component={TeacherList} options={{
+                tabBarLabel: "Proffys",
+                tabBarIcon: ({ color, size }) => {
+                    return (
+                        <Ionicons name="ios-easel" size={size} color={color} />
+                    );
+                }
+            }}></Screen>
+            <Screen name="Favorite" component={Favorite} options={{
+                tabBarLabel: "Proffys",
+                tabBarIcon: ({ color, size }) => {
+                    return (
+                        <Ionicons name="ios-heart" size={size} color={color} />
+                    );
+                }
+            }}></Screen>
         </Navigator>
     );
 }
