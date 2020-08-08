c7479dbaf5a544a1b5f88d4da801ce445c544a67
apfdamascena <apfd@cin.ufpe.br>
2020-08-07 22:08:33 -0300

    favorit list ok

--- mobile/src/Pages/Favorites/index.tsx
+++ mobile/src/Pages/Favorites/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { View, ScrollView } from 'react-native';
 import styles from './styles';
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem/TeacherItem';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default function Favorite(){
+    const [favorites, setFavorites] = useState([]);
+
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then((response) => {
+            if(response){
+                const favoritedTeachers = JSON.parse(response);
+                setFavorites(favoritedTeachers);
+            }
+        });
+    }
+
+    useEffect(() => {
+        loadFavorites();
+    },[])
     return(
         <View style = {styles.container}>
             <PageHeader title =  "Meus proffys favoritos"/>
@@ -15,12 +30,11 @@ export default function Favorite(){
                     paddingBottom: 16,
                 }}
             >
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+            {favorites.map((teacher: Teacher) => {
+                return(
+                    <TeacherItem teacher = {teacher} favorited/>
+                );
+            })}
             </ScrollView>
         </View>
     );
